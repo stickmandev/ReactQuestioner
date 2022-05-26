995780cf09e6c16f4a450012b669b81d478306c6
stickmandev <stickmandeveloper@gmail.com>
2022-05-26 02:49:29 +0100

    progress bar created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react'
-import logo from './logo.svg'
+import React, {useState, useEffect} from 'react'
 import './App.css'
+import data from "./questions.json"  
 
 function App () {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+    // const [numOfQuestions, set] = React.useState([]);
+    // const [numOfQuestions, set] = React.useState(0);
+    const [numOfQuestions, setNumOfQuestions] = React.useState(0);
+    const [currentNum, setCurrentNum] = React.useState(1);
+    const [progress, setProgress] = React.useState(0);
+
+    const nextQuestion =()=>{
+        if(currentNum<numOfQuestions){
+            setCurrentNum(currentNum+1)
+        }
+        
+    }
+
+    useEffect(() => {
+        setNumOfQuestions(data.length)
+        setProgress((currentNum*100)/numOfQuestions)
+
+        console.log(currentNum)
+    }, [currentNum, progress]);
+    
+    
+
+    return (
+        <div className='App'>
+            <div id='progressBar' style={{width:`${progress}%`}}></div>
+
+            <div>
+                <h1></h1>
+
+                <small></small>
+
+                <div id='starOverlay'>
+                    <span className='stars'></span>
+                </div>
+
+                <p className='question'></p>
+
+                <div id='answers'>
+                    
+                </div>
+
+                <h2 id='answerStatus'></h2>
+
+                <button id='nextQuestion' onClick={nextQuestion}>next</button>
+
+                <div id='score'></div>
+            </div>
+        </div>
+    )
 }
 
 export default App
