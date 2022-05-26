2861808b99b2b50c5afcb95df9281d95fd439303
stickmandev <stickmandeveloper@gmail.com>
2022-05-26 18:35:13 +0100

    rounding up8

--- build/precache-manifest.a27768af6989a4fd578a2360761b1d40.js
+++ build/precache-manifest.a27768af6989a4fd578a2360761b1d40.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "343ba658ae3394ea6a5515b0d23178a7",
+    "revision": "ca68caeaebe464483450c2f14c2d639b",
     "url": "/ReactQuestioner/index.html"
   },
   {
-    "revision": "d5e849d1393ad1bbced1",
-    "url": "/ReactQuestioner/static/css/main.5a8d01a3.chunk.css"
+    "revision": "a2ac28286cb8171c66e0",
+    "url": "/ReactQuestioner/static/css/main.add9795c.chunk.css"
   },
   {
     "revision": "f319fe71299dd408b275",
     "url": "/ReactQuestioner/static/js/2.ad3586fe.chunk.js"
   },
   {
-    "revision": "d5e849d1393ad1bbced1",
-    "url": "/ReactQuestioner/static/js/main.1836e0c3.chunk.js"
+    "revision": "a2ac28286cb8171c66e0",
+    "url": "/ReactQuestioner/static/js/main.5e52ec91.chunk.js"
   },
   {
     "revision": "61b6e173e9c88f7a924d",
--- src/App copy.js
+++ src/App copy.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useMemo} from 'react'
 import './App.css'
 import data from "./questions.json"  
 import star1 from "./star1.png"  
@@ -20,7 +20,8 @@ function App () {
     const [answeredFailed, setAnsweredFailed] = React.useState(0);
     const [score, setScore] = React.useState(0);
     const [maxScore, setMaxScore] = React.useState(0);
-    // const [numOfQuestions, set] = React.useState(0);
+    const [answersArr, setAnswersArr] = React.useState([]);
+        // const [numOfQuestions, set] = React.useState(0);
     
 
     const nextQuestion =()=>{
@@ -40,10 +41,7 @@ function App () {
             setAnswerStatus("Sorry!")
             setAnsweredFailed(answeredFailed+1)
         }
-        const answerClass = document.getElementsByClassName("answers")
-        for(let i=0; i<answerClass.length; i++){
-            answerClass[i].disabled = true
-        }
+        setDisabledBTN(true)
         setnextBTN(false)
         
     }
@@ -87,31 +85,6 @@ function App () {
                 break;
         }
 
-        const answersArr = []
-        data[dataId].incorrect_answers.map(
-            (answers, pk, answersObj)=>{
-                answersArr.push(
-                    <button className='answers' onClick={validateAns} disabled={disabledBTN}>{answers.replace(/%3A%20|%20/g," ").replace(/%27|%3F|%22|%2C|%24/g,"")}</button>
-                )
-            }
-        )
-
-        answersArr.push(
-            <button className='answers' onClick={validateAns} disabled={disabledBTN}>{data[dataId].correct_answer.replace(/%3A%20|%20/g," ").replace(/%27|%3F|%22|%2C|%24/g,"")}</button>
-        )
-
-        const shuffleArray = array => {
-            for (let i = array.length - 1; i > 0; i--) {
-              const j = Math.floor(Math.random() * (i + 1));
-              const temp = array[i];
-              array[i] = array[j];
-              array[j] = temp;
-            }
-        }
-
-        shuffleArray(answersArr)
-        setAnswers(answersArr)
-
         setScore(
             (answeredCorrect*100)/numOfQuestions
         )
@@ -119,10 +92,34 @@ function App () {
         setMaxScore(
             100-((answeredFailed*100)/numOfQuestions)
         )
-        // console.log(answers);
-    }, [currentNum, progress, disabledBTN]);
+    }, [currentNum, progress]);
     
+    useEffect(() => {
+        const answersSubArr = [<button className='answers' onClick={validateAns} disabled={disabledBTN}>{data[dataId].correct_answer.replace(/%3A%20|%20/g," ").replace(/%27|%3F|%22|%2C|%24/g,"")}</button>]
+        data[dataId].incorrect_answers.map(  
+           (answers, pk, answersObj)=>{
+               answersSubArr.push(<button className='answers' onClick={validateAns} disabled={disabledBTN}>{answers.replace(/%3A%20|%20/g," ").replace(/%27|%3F|%22|%2C|%24/g,"")}</button>)
+           }
+       )
+       setAnswersArr(answersSubArr)
+    }, []);
+
+    useMemo(
+        ()=>{
+            console.log(answersArr)
+            const shuffleArray = array => {
+                for (let i = array.length - 1; i > 0; i--) {
+                  const j = Math.floor(Math.random() * (i + 1));
+                  const temp = array[i];
+                  array[i] = array[j];
+                  array[j] = temp;
+                }
+              }
     
+            shuffleArray(answersArr)
+            setAnswers(answersArr)
+        }, [progress]
+    )
 
     return (
         <div className='App'>
